87ca31b952187e60fa9a21c91d03611d0beda1ce
Tadeo Donaire <u202214197@upc.edu.pe>
2025-12-02 23:08:46 -0500

    MEJORA EN COSITAS

---
+++ src/app/components/inicio/simulaciones-anteriores/simulaciones-anteriores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulaciones-anteriores',
+  imports: [],
+  templateUrl: './simulaciones-anteriores.component.html',
+  styleUrl: './simulaciones-anteriores.component.css'
+})
+export class SimulacionesAnterioresComponent {
+
+}
